bb2a57da5cdc99a3a3cc16aedc88031d8c268ba3
Спиридонов Павел Александрович <ip715s22@mwp09.csc.local>
2018-05-10 13:48:11 +0700

    Add tests

--- test/main.c
+++ test/main.c
@@ -28,6 +28,43 @@ CTEST(PawnMove, Incorrect) {
     int expected = 0;
     ASSERT_EQUAL(expected,result);
 }
+CTEST(KnightMove, Correct) {
+    int result = make_move("Nb1-a3",0,0);
+    int expected = 1;
+    ASSERT_EQUAL(expected,result);
+}
+
+CTEST(KnightMove, Incorrect) {
+    int result = make_move("ng1-h3",0,0);
+    int expected = 0;
+    ASSERT_EQUAL(expected,result);
+}
+
+CTEST(BishopMove, Correct) {
+    int result = make_move("Bc1-d2",0,0);
+    int expected = 1;
+    ASSERT_EQUAL(expected,result);
+}
+
+CTEST(BishopMove, Incorrect) {
+    int result = make_move("Bg8-h7",0,0);
+    int expected = 0;
+    ASSERT_EQUAL(expected,result);
+}
+
+CTEST(QueenMove, Correct) {
+    int result = make_move("Qd1-f3",0,0);
+    int expected = 1;
+    ASSERT_EQUAL(expected,result);
+}
+
+CTEST(QueenMove, Incorrect) {
+    int result = make_move("Qd8-d7",0,0);
+    int expected = 0;
+    ASSERT_EQUAL(expected,result);
+}
+
+
 
 int main(int argc, const char** argv) {
     init_board();
